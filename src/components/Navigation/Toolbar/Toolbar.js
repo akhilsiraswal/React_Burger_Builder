ac918d531b2f65471370f31d795f6a72e58066d0
akhilsiraswal <akhilsiraswal@gmail.com>
2021-04-10 09:57:13 +0530

    -9-04-21

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import classes from './Toolbar.css'
- 
-const toolbar = (props) =>(
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <div>LOGO</div>
-        <nav>
-            ... 
-        </nav> 
-    </header>
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./Toolbar.css";
+
+const toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <Logo height="80%" />
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
